bc11d8c9524346c94a5f00aca053a276d1d9207f
Lyndon Smith <lyndon@infomediauk.net>
2015-02-24 17:07:39 +0000

    Formatted.

--- src/main/java/stripesbook/action/HelloActionBean.java
+++ src/main/java/stripesbook/action/HelloActionBean.java
@@ -17,26 +17,41 @@ import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
 
-public class HelloActionBean implements ActionBean {/* (1) */
-    private ActionBeanContext ctx;
-    public ActionBeanContext getContext() { return ctx; }
-    public void setContext(ActionBeanContext ctx) { this.ctx = ctx; }
+public class HelloActionBean implements ActionBean
+{/* (1) */
+  private ActionBeanContext ctx;
 
-    private Date date;/* (2) */
-    public Date getDate() {
-        return date;
-    }
-    @DefaultHandler
-    public Resolution currentDate() {/* (3) */
-        date = new Date();
-        return new ForwardResolution(VIEW);
-    }
-    public Resolution randomDate() {
-        long max = System.currentTimeMillis();
-        long random = new Random().nextLong() % max;
-        date = new Date(random);
-        return new ForwardResolution(VIEW);
-    }
-    private static final String VIEW = "/WEB-INF/jsp/hello.jsp";
+  public ActionBeanContext getContext()
+  {
+    return ctx;
+  }
+
+  public void setContext(ActionBeanContext ctx)
+  {
+    this.ctx = ctx;
+  }
+
+  private Date date;/* (2) */
+
+  public Date getDate()
+  {
+    return date;
+  }
+
+  @DefaultHandler
+  public Resolution currentDate()
+  {/* (3) */
+    date = new Date();
+    return new ForwardResolution(VIEW);
+  }
+
+  public Resolution randomDate()
+  {
+    long max = System.currentTimeMillis();
+    long random = new Random().nextLong() % max;
+    date = new Date(random);
+    return new ForwardResolution(VIEW);
+  }
+
+  private static final String VIEW = "/WEB-INF/jsp/hello.jsp";
 }
-
