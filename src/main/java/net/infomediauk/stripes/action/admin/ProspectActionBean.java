cd89cbf144399787e29ae90b4609997a3da7f4eb
Lyndon Smith <lyndon@infomediauk.net>
2015-03-13 14:14:21 +0000

    Removed unnecessary imports.

--- src/main/java/net/infomediauk/stripes/action/admin/ProspectActionBean.java
+++ src/main/java/net/infomediauk/stripes/action/admin/ProspectActionBean.java
@@ -1,20 +1,9 @@
 package net.infomediauk.stripes.action.admin;
 
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 
-import javax.ws.rs.core.MediaType;
-
-import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.core.util.MultivaluedMapImpl;
-import com.sun.jersey.multipart.BodyPart;
-import com.sun.jersey.multipart.MultiPart;
 
 import stripesbook.action.BaseActionBean;
 import net.infomediauk.dao.impl.XmlDisciplineDao;
