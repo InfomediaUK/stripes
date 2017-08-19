2b29eae62de2bc04b5f23688366e04c6c33048ad
Lyndon Smith <lyndon@infomediauk.net>
2017-08-19 13:30:37 +0100

    HTML5 new stuff...

---
+++ src/main/java/net/infomediauk/stripes/tag/Html5InputText.java
@@ -0,0 +1,37 @@
+package net.infomediauk.stripes.tag;
+
+import net.sourceforge.stripes.tag.InputTextTag;
+
+public class Html5InputText extends InputTextTag
+{
+
+  public void setRequired(String required)
+  {
+    set("required", required);
+  }
+
+  public String getRequired()
+  {
+    return get("required");
+  }
+
+  public String getPlaceholder()
+  {
+    return get("placeholder");
+  }
+
+  public void setPlaceholder(String placeholder)
+  {
+    set("placeholder", placeholder);
+  }
+
+  public String getPattern()
+  {
+    return get("pattern");
+  }
+
+  public void setPattern(String pattern)
+  {
+    set("pattern", pattern);
+  }
+}
