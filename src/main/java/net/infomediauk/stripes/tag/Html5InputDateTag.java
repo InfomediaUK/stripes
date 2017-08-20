056b71f31bc050a06803258e11ed50ad9cdcb656
Lyndon Smith <lyndon@infomediauk.net>
2017-08-20 10:57:08 +0100

    Change attribute type to value data in constructor...

--- src/main/java/net/infomediauk/stripes/tag/Html5InputDateTag.java
+++ src/main/java/net/infomediauk/stripes/tag/Html5InputDateTag.java
@@ -2,4 +2,11 @@ package net.infomediauk.stripes.tag;
 
 public class Html5InputDateTag extends Html5InputTextTag
 {
+
+  public Html5InputDateTag()
+  {
+    super();
+    getAttributes().put("type", "date");
+  }
+
 }
