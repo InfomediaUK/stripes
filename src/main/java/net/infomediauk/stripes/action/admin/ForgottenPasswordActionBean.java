c0498477ff63d1b67ffaa2e68cef15f70b2b330c
Lyndon Smith <lyndon@infomediauk.net>
2017-08-21 16:50:57 +0100

    New action...

--- src/main/java/net/infomediauk/stripes/action/admin/ForgottenPasswordActionBean.java
+++ src/main/java/net/infomediauk/stripes/action/admin/ForgottenPasswordActionBean.java
@@ -1,7 +1,19 @@
 package net.infomediauk.stripes.action.admin;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.apache.commons.lang3.StringUtils;
+
 import stripesbook.action.BaseActionBean;
 import net.infomediauk.dao.impl.XmlUserDao;
+import net.infomediauk.mail.CustomMimeMessage;
+import net.infomediauk.mail.SMTPAuthenticator;
 import net.infomediauk.model.User;
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.DontValidate;
@@ -19,6 +31,11 @@ public class ForgottenPasswordActionBean extends BaseActionBean
   private static final String FORM = "/WEB-INF/jsp/admin/forgottenPassword.jsp";
   private String email;
 
+  public String getEmail()
+  {
+    return email;
+  }
+
   public void setEmail(String userName)
   {
     this.email = userName;
@@ -34,7 +51,56 @@ public class ForgottenPasswordActionBean extends BaseActionBean
   public Resolution sendPasswordHint() throws Exception
   {
     System.out.println("In sendPasswordHint...");
-    return new RedirectResolution(LoginActionBean.class);
+    User user = XmlUserDao.getInstance().selectByEmail(email);
+    String from = "admin@matchmyjob.co.uk";
+    Properties props = new Properties();
+    props.put("mail.smtp.host", "smtp.easily.co.uk");
+    props.put("mail.smtp.port", "1025");
+    props.setProperty("mail.transport.protocol", "smtp");
+    props.setProperty("mail.smtp.auth", "true");
+    SMTPAuthenticator authenticator = new SMTPAuthenticator("cgg0dhqh51j0", "helmet22");
+    Session session = Session.getInstance(props, authenticator);
+    session.setDebug(true);
+    try 
+    {
+      Transport transport = null;
+      try
+      {
+        transport = session.getTransport("smtp");
+        // Create a message.
+        MimeMessage mimeMessage = new CustomMimeMessage(session);
+        // Added header stuff. 23/07/2016 Lyndon
+        mimeMessage.addHeader("Content-type", "text/HTML; charset=UTF-8");
+        if (StringUtils.isEmpty(from)) 
+        {
+          mimeMessage.setFrom();
+        } 
+        else 
+        {
+          mimeMessage.setFrom(new InternetAddress(from));
+        }
+        InternetAddress[] toInternetAddresses = InternetAddress.parse(email);
+        mimeMessage.addRecipients(Message.RecipientType.TO, toInternetAddresses);
+        mimeMessage.setSubject("Your HINT", "UTF-8");
+        mimeMessage.setText(user.getPasswordHint(), "UTF-8");
+        mimeMessage.setSentDate(new java.util.Date());
+        transport.connect();
+        // Send the mail.
+        transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+      }
+      finally
+      {
+        if (transport != null)
+        {
+          transport.close();
+        }
+      }
+    } 
+    catch (Exception e) 
+    {
+      System.out.println(e.getMessage());
+    }
+    return new RedirectResolution(ForgottenPasswordActionBean.class, "forgottenPasswordHintSent").addParameter("email", email);
   }
   
   @ValidationMethod(on="sendPasswordHint", priority=0)
@@ -46,4 +112,11 @@ public class ForgottenPasswordActionBean extends BaseActionBean
       errors.addGlobalError(new SimpleError("User NOT found for email address."));  
     }
   }
+  
+  @DontValidate
+  public Resolution forgottenPasswordHintSent()
+  {
+    return new ForwardResolution("/WEB-INF/jsp/admin/forgottenPasswordHintSent.jsp");
+  }
+
 }
