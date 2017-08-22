3da2b0b17b6ce5070e0a5a5912e72fc7ff3bd04a
Lyndon Smith <lyndon@infomediauk.net>
2017-08-22 15:59:19 +0100

    New place for admin constants...

---
+++ src/main/java/net/infomediauk/stripes/action/admin/Constants.java
@@ -0,0 +1,7 @@
+package net.infomediauk.stripes.action.admin;
+
+public class Constants
+{
+  public final static String EMAIL_HOST = "Email Host";
+  public final static String EXCHANGE_SERVER = "Exchange Server";
+}
