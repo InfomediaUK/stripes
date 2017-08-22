a4125c6c07db5e386b44b16d0c83b889184fb4c4
Lyndon Smith <lyndon@infomediauk.net>
2017-08-22 14:13:10 +0100

    Removed sendgrid stuff...

--- src/main/java/net/infomediauk/stripes/action/admin/SendEmailActionBean.java
+++ src/main/java/net/infomediauk/stripes/action/admin/SendEmailActionBean.java
@@ -1,7 +1,5 @@
 package net.infomediauk.stripes.action.admin;
 
-import com.sendgrid.SendGrid;
-
 import stripesbook.action.BaseActionBean;
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
@@ -14,15 +12,6 @@ public class SendEmailActionBean extends BaseActionBean
   @DefaultHandler
   public Resolution view() throws Exception
   {
-    SendGrid sendgrid = new SendGrid("ebookshot", "ebookshot26");
-    SendGrid.Email email = new SendGrid.Email();
-
-    email.addTo("lyndon@infomediauk.net");
-    email.setFrom("admin@ebookshop.com");
-    email.setSubject("Sending with SendGrid is Fun");
-    email.setHtml("and easy to do anywhere, even with Java");
-
-    SendGrid.Response response = sendgrid.send(email); 
     return new ForwardResolution(FORM);
   }
   
